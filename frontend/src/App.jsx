50699807a245a9195f21c02405e2ed879e74e772
vivekmpatne <vivekpatnem@gmail.com>
2026-04-26 16:57:47 +0530

    updated history and dailylog in App jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import History from "./pages/History";
+import DailyLog from "./pages/DailyLog";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -14,9 +16,12 @@ function App() {
       <Router>
         <Toaster />
         <Routes>
+
+          {/* Public Routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
 
+          {/* Protected Routes */}
           <Route
             path="/dashboard"
             element={
@@ -26,7 +31,30 @@ function App() {
             }
           />
 
-          <Route path="*" element={<Login />} />
+          <Route
+            path="/history"
+            element={
+              <ProtectedRoute>
+                <History />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/daily-log"
+            element={
+              <ProtectedRoute>
+                <DailyLog />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Default Route */}
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+
+          {/* Fallback */}
+          <Route path="*" element={<Navigate to="/login" />} />
+
         </Routes>
       </Router>
     </AuthProvider>
